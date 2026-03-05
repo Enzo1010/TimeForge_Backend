d020d5b8da5e8e431c4599ff893888645166ec92
Computação <zikimuller017@gmail.com>
2026-03-04 22:05:23 -0300

    feat(slothorarios): crud de slot_horarios

--- timeforge-api/src/main/java/br/com/timeforge/timeforge_api/controller/SlotHorarioController.java
+++ timeforge-api/src/main/java/br/com/timeforge/timeforge_api/controller/SlotHorarioController.java
@@ -1,11 +1,44 @@
 package br.com.timeforge.timeforge_api.controller;
 
+import br.com.timeforge.timeforge_api.domain.SlotHorario;
 import br.com.timeforge.timeforge_api.repository.SlotHorarioRepository;
+import br.com.timeforge.timeforge_api.service.SlotHorarioService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/")
-class SlotHorarioController {
+import java.util.List;
+
+@RestController
+@RequestMapping("/slothorarios")
+public class SlotHorarioController {
+
   private final SlotHorarioService service;
+
+  public SlotHorarioController(SlotHorarioService service) {
+    this.service = service;
+  }
+
+  @GetMapping("/listar")
+  public List<SlotHorario> listarSlotHorarios(){
+    return service.listarSlotHorarios();
+  }
+
+  @PostMapping("/gravar")
+  public SlotHorario gravarSlotHorario(@RequestBody SlotHorario slotHorarioObject){
+    return service.gravarSlotHorario(slotHorarioObject);
+  }
+
+  @PatchMapping("/editar/{id}")
+  public SlotHorario editarSlotHorario(@PathVariable Long id, @RequestBody SlotHorario slotHorarioObject){
+    return service.editarSlotHorario(id, slotHorarioObject);
+  }
+
+  @DeleteMapping("/deletar/{id}")
+  public ResponseEntity<String> deletarSlotHorario(@PathVariable Long id){
+    service.excluirSlotHorario(id);
+    return ResponseEntity.ok("Slot de horario excluido com sucesso!");
+  }
+
+
 }
--- timeforge-api/src/main/java/br/com/timeforge/timeforge_api/service/SlotHorarioService.java
+++ timeforge-api/src/main/java/br/com/timeforge/timeforge_api/service/SlotHorarioService.java
@@ -3,21 +3,53 @@ package br.com.timeforge.timeforge_api.service;
 import br.com.timeforge.timeforge_api.domain.SlotHorario;
 import br.com.timeforge.timeforge_api.repository.SlotHorarioRepository;
 import org.aspectj.weaver.patterns.ConcreteCflowPointcut;
+import org.hibernate.annotations.NotFound;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-class SlotHorarioService {
+public class SlotHorarioService {
 //
   private final SlotHorarioRepository repository;
 
-  SlotHorarioService(SlotHorarioRepository repository) {
+  public SlotHorarioService(SlotHorarioRepository repository) {
     this.repository = repository;
   }
 
-  public List<SlotHorario> listarHorarios(){
-    return    repository.findAll();
+  public List<SlotHorario> listarSlotHorarios(){
+    return repository.findAll();
+  }
+
+  public SlotHorario gravarSlotHorario(@RequestBody SlotHorario slotHorarioObject){
+    return repository.save(slotHorarioObject);
+  }
+
+  public SlotHorario editarSlotHorario(Long id, SlotHorario slotHorarioObject){
+    SlotHorario slotHorarioEncontrado = repository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Slot de horario com id (" + id + ") não encontrado"));
+
+    slotHorarioEncontrado.setDiaSemana(slotHorarioObject.getDiaSemana());
+    slotHorarioEncontrado.setHoraInicio(slotHorarioObject.getHoraInicio());
+    slotHorarioEncontrado.setHoraFim(slotHorarioObject.getHoraFim());
+
+    return repository.save(slotHorarioEncontrado);
+  }
+
+  public ResponseEntity<String> excluirSlotHorario(Long id){
+    SlotHorario slotHorarioEncontrado = repository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Slot de horario com id (" + id + ") não encontrado"));
+
+    repository.delete(slotHorarioEncontrado);
+
+    return ResponseEntity.ok("Slot de horario excluido com sucesso!");
   }
 
 }
