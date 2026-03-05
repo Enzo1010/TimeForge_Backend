5227f468ad12c41b2c3faa28aeebac34909eed50
Computação <zikimuller017@gmail.com>
2026-03-04 21:27:52 -0300

    refactor(project): ajuste identacao pra 2 espacos

---
+++ timeforge-api/src/main/java/br/com/timeforge/timeforge_api/controller/SlotHorarioController.java
@@ -0,0 +1,10 @@
+package br.com.timeforge.timeforge_api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+class SlotHorario {
+
+}
---
+++ timeforge-api/src/main/java/br/com/timeforge/timeforge_api/service/SlotHorarioService.java
@@ -0,0 +1,8 @@
+package br.com.timeforge.timeforge_api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class SlotHorarioService {
+
+}
