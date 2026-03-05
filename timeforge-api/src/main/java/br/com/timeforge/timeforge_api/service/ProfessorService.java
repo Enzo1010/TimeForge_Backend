f09ffae334a43b033f0ffb9edc5944f6584011b8
Computação <zikimuller017@gmail.com>
2026-03-04 21:06:28 -0300

    feat(professores): CRUD professores OK

--- timeforge-api/src/main/java/br/com/timeforge/timeforge_api/service/ProfessorService.java
+++ timeforge-api/src/main/java/br/com/timeforge/timeforge_api/service/ProfessorService.java
@@ -4,6 +4,7 @@ import br.com.timeforge.timeforge_api.domain.Professor;
 import br.com.timeforge.timeforge_api.repository.ProfessorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -17,5 +18,30 @@ public class ProfessorService {
 
     public List<Professor> listarProfessores() {
         return repository.findAll();
+    }
+
+    public Professor listarProfessorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor com id (" + id + ") não encontrado!"));
+    }
+
+    public Professor cadastrarProfessor(Professor professorObject) {
+        return repository.save(professorObject);
+    }
+
+    public Professor editarProfessor(Long id, Professor professorObject){
+        Professor professorEncontrado = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado!"));
+
+        professorEncontrado.setNome(professorObject.getNome());
+
+        return repository.save(professorEncontrado);
+    }
+
+    public void deletarProfessor(Long id){
+        Professor professorEncontrado = repository.findById(id)
+                        .orElseThrow(() -> new RuntimeException("Professor não encontrado!"));
+
+        repository.delete(professorEncontrado);
     }
 }
